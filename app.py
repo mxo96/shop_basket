15849f315355afaffebc72be57594ac902fad5da
mirko.caruso <mirko.caruso@jobrapido.com>
2020-03-10 21:00:03 +0100

    app with all routing rules

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+from utils.extract_basket import extract_shop_basket
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/receipt', methods=['POST'])
+def shop_receipt():
+    basket = extract_shop_basket(request.form.to_dict())
+    return render_template('receipt.html', basket=basket)
+
+
+if __name__ == '__main__':
+    app.run()
